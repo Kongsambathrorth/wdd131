0795f402ac641c313ce848e83551c4be871e5fdc
Sambathrorth <kongsambathrorth@gmail.om>
2026-01-10 19:47:45 +0700

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,6 @@
-document.getElementById("lastModified").innerHTML = document.lastModified;
+// Output the current copyright year
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+// Output the date the document was last modified
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
